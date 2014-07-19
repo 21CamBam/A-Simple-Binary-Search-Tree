6df107746f0bd99c6acd18f7381700e6c3117693
Cammi Smith <cammi.smith@email.wsu.edu>
2014-07-18 17:39:42 -0700

    Create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+// The following code is the base code given to me by my professor to run my program
+// Please note that it is not mine
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <stdio.h>
+#include "BST.h"
+
+void MenuMode();
+void ProcessLine(HW2BST& tree, char* line);
+
+#endif
